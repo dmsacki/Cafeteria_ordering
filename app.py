394c0ae82213186703cdca410c262d4fedaf81ff
machakelvin <machakelvin76@gmail.com>
2024-09-25 14:15:24 +0300

    some changes

--- app.py
+++ app.py
@@ -1,21 +1,3 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/menu')
-def menu():
-    return render_template('menu.html')
-
-@app.route('/order')
-def order():
-    return render_template('order.html')
-
-if __name__ == "__main__":
-    app.run()
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 app = Flask(__name__)
